b59c5b073944c434f41a3af6d533ae6f1b525b78
Administrator <admin@example.com>
2020-02-27 22:56:23 +0800

    修改项目名称为子模块形式

--- readme.md
+++ readme.md
@@ -1,2 +1,3 @@
-"" 
-"# jirebaRmrb" 
+# NLP-learning
+
+## 001day-数据处理-人民日报数据处理
--- rmrb/rmrbCut.py
+++ rmrb/rmrbCut.py
@@ -21,31 +21,42 @@ class TraversalFun():
         self.rootDir = rootDir
 
     def TraversalDir(self):
-        TraversalFun.AllFiles(self, self.rootDir)
+        return TraversalFun.getCorpus(self, self.rootDir)
 
-    def AllFiles(self, rootDir):
+    def getCorpus(self, rootDir):
+        corpus = []
         for file in os.listdir(rootDir):
             path  = os.path.join(rootDir, file)
             if os.path.isfile(path):
                 # 打印文件地址
-                print(os.path.abspath(path))
+                #print(os.path.abspath(path))
                 # 获取文章内容，fromfile主要用来处理数组 pass
                 # filecontext = np.fromfile(os.path.abspath(path))
                 with open(os.path.abspath(path), "r", encoding='utf-8') as file:
                     filecontext = file.read();
-                    print(filecontext)
-                    # 分词
-                    seg_list = jieba.cut(filecontext, cut_all=True)
-                    print("[精确模式]：" + "/".join(seg_list))
+                    #print(filecontext)
+                    # 分词,先去除空格
+                    filecontext = filecontext.strip("\n").replace(" ", "")
+                    seg_list = jieba.lcut_for_search(filecontext)
+                    corpus += seg_list
+                    #print(seg_list)
+                    #print("[精确模式]：" + "/".join(seg_list))
             elif os.path.isdir(path):
                 TraversalFun.AllFiles(self, path)
+        return corpus
+
+
+#构造词典，统计每个词的频率
+def build_dict(corpus):
+    pass
 
 
 if __name__ == "__main__":
     startTime = time.time()
     tra = TraversalFun("./1946年05月")
-    # tra = TraversalFun("./rmrb")
-
-    tra.TraversalDir()
+    corpus = tra.TraversalDir()
+    print("数据集大小，size: " + str(len(corpus)))
+    #print(corpus)
+    build_dict(corpus)
     endTime = time.time()
     print("total cost time(s)", (endTime-startTime), 's')
