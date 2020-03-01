38c7033e3940fb7f0a42574af213388d67bf482e
Administrator <admin@example.com>
2020-03-01 10:09:15 +0800

    统计每个词的TF值，并计算信息熵

--- dataprocess-rmrb/rmrbCut.py
+++ dataprocess-rmrb/rmrbCut.py
@@ -1,11 +1,11 @@
 #coding=utf-8
 
-import jieba
-import jieba.posseg as pseg
-import jieba.analyse as anls
-import numpy as np
+import os
+import time
 
-import  os,time
+import jieba
+import re
+from math import log
 
 # 1、递归读取文件
 # 2、读取到文件，文件内容转换成文本，使用jieba进行分词
@@ -25,6 +25,7 @@ class TraversalFun():
 
     def getCorpus(self, rootDir):
         corpus = []
+        r1 = u'[a-zA-Z0-9’!"#$%&\'()*+,-./:：;<=>?@，。?★、…【】《》？“”‘’！[\\]^_`{|}~]+'  # 用户也可以在此进行自定义过滤字符
         for file in os.listdir(rootDir):
             path  = os.path.join(rootDir, file)
             if os.path.isfile(path):
@@ -35,8 +36,10 @@ class TraversalFun():
                 with open(os.path.abspath(path), "r", encoding='utf-8') as file:
                     filecontext = file.read();
                     #print(filecontext)
-                    # 分词,先去除空格
-                    filecontext = filecontext.strip("\n").replace(" ", "")
+                    # 分词 去掉符号
+                    filecontext = re.sub(r1, '', filecontext)
+                    filecontext = filecontext.replace("\n", '')
+                    filecontext = filecontext.replace(" ", '')
                     seg_list = jieba.lcut_for_search(filecontext)
                     corpus += seg_list
                     #print(seg_list)
@@ -46,9 +49,28 @@ class TraversalFun():
         return corpus
 
 
-#构造词典，统计每个词的频率
-def build_dict(corpus):
-    pass
+#构造词典，统计每个词的频率，并计算信息熵
+def calc_tf(corpus):
+    #   统计每个词出现的频率
+    word_freq_dict = dict()
+    for word in corpus:
+        if word not in word_freq_dict:
+            word_freq_dict[word] = 1
+        word_freq_dict[word] += 1
+    # 将这个词典中的词，按照出现次数排序，出现次数越高，排序越靠前
+    word_freq_dict = sorted(word_freq_dict.items(), key=lambda x:x[1], reverse=True)
+    # 计算TF概率
+    word_tf = dict()
+    # 信息熵
+    shannoEnt = 0.0
+    # 按照频率，从高到低，开始遍历，并未每个词构造一个id
+    for word, freq in word_freq_dict:
+        # 计算p(xi)
+        prob = freq / len(corpus)
+        word_tf[word] = prob
+        shannoEnt -= prob*log(prob, 2)
+    print(len(word_freq_dict))
+    return word_tf, shannoEnt
 
 
 if __name__ == "__main__":
@@ -57,6 +79,9 @@ if __name__ == "__main__":
     corpus = tra.TraversalDir()
     print("数据集大小，size: " + str(len(corpus)))
     #print(corpus)
-    build_dict(corpus)
+    word_tf, shannoEnt = calc_tf(corpus)
+    print("信息熵：" + str(shannoEnt))
+    print("TF: ")
+    print(word_tf)
     endTime = time.time()
     print("total cost time(s)", (endTime-startTime), 's')
