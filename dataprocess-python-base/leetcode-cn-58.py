1732f144acdd5391a7e37a81b9dfdaabd61ecf95
Administrator <admin@example.com>
2020-03-10 20:12:16 +0800

    截取解法

--- dataprocess-python-base/leetcode-cn-58.py
+++ dataprocess-python-base/leetcode-cn-58.py
@@ -3,28 +3,9 @@ class Solution:
         pass
 
     def reverseLeftWords(self, strings: str, n: int):
-        print(strings)
-        for i in range(n):
-
-            print("移动" + str(i) + "个字符之前：" + strings)
-            strings = strings + strings[0]
-            print(type(strings))
-            strings = self.removeIndex(strings, 0)
-            print("==" + strings)
-            strings = str(strings)
-            print("移动" + str(i) + "个字符之后：" + strings)
-
-    def removeIndex(self, test_str:str, index:int):
-
-        # 接收移除后的字符串
-        new_str = ""
-        for i in range(0, len(test_str)):
-            if i != index:
-                new_str = new_str + test_str[i]
-        return new_str
+        return strings[n:len(strings)] + strings[0:n]
 
 
 s = Solution()
-# test_str = "Runoob"
-# s.removeIndex(test_str, 2)
-s.reverseLeftWords("abcdefg", 2)
+strings = s.reverseLeftWords("abcdefg", 2)
+print(strings)
