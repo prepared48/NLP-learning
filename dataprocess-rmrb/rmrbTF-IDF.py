1a2aa8c50f938af79314614d24ababca525c8c4d
Administrator <admin@example.com>
2020-03-16 21:58:41 +0800

    计算TF-IDF

--- dataprocess-rmrb/rmrbTF-IDF.py
+++ dataprocess-rmrb/rmrbTF-IDF.py
@@ -2,6 +2,9 @@
 
 import os
 import time
+from collections import Counter,defaultdict
+import math
+from collections import defaultdict
 
 import jieba
 import re
@@ -24,11 +27,13 @@ class TraversalFun():
         return TraversalFun.getCorpus(self, self.rootDir)
 
     def getCorpus(self, rootDir):
-        corpus = []
+
         r1 = u'[a-zA-Z0-9’!"#$%&\'()*+,-./:：;<=>?@，。?★、…【】《》？“”‘’！[\\]^_`{|}~]+'  # 用户也可以在此进行自定义过滤字符
         listdir = os.listdir(rootDir)
+        corpus = []
+        i = 0
         for file in listdir:
-            path  = os.path.join(rootDir, file)
+            path = os.path.join(rootDir, file)
             if os.path.isfile(path):
                 # 打印文件地址
                 #print(os.path.abspath(path))
@@ -41,49 +46,69 @@ class TraversalFun():
                     filecontext = re.sub(r1, '', filecontext)
                     filecontext = filecontext.replace("\n", '')
                     filecontext = filecontext.replace(" ", '')
-                    seg_list = jieba.cut(filecontext, cut_all=True)
-                    corpus += seg_list
-                    #print(seg_list)
+                    seg_list = jieba.lcut(filecontext, cut_all=True)
+                    # corpus[i] = seg_list
+                    # print(seg_list)
+                    corpus.append(seg_list)
                     #print("[精确模式]：" + "/".join(seg_list))
             elif os.path.isdir(path):
                 TraversalFun.AllFiles(self, path)
+            i += 1
+        # print(corpus)
         return corpus
 
-
-#构造词典，统计每个词的频率，并计算信息熵
+#构造词典，统计每个词的频率
 def calc_tf(corpus):
     #   统计每个词出现的频率
-    word_freq_dict = dict()
-    for word in corpus:
-        if word not in word_freq_dict:
-            word_freq_dict[word] = 1
-        word_freq_dict[word] += 1
-    # 将这个词典中的词，按照出现次数排序，出现次数越高，排序越靠前
-    word_freq_dict = sorted(word_freq_dict.items(), key=lambda x:x[1], reverse=True)
-    # 计算TF概率
-    word_tf = dict()
-    # 信息熵
-    shannoEnt = 0.0
-    # 按照频率，从高到低，开始遍历，并未每个词构造一个id
-    for word, freq in word_freq_dict:
-        # 计算p(xi)
-        prob = freq / len(corpus)
-        word_tf[word] = prob
-        shannoEnt -= prob*log(prob, 2)
-    return word_tf, shannoEnt
+    tf = []
+    for doc in corpus:
+        tf.append(Counter(doc))
+    return tf
 
 
+def calc_idf(word_freq_dict):
+    idf = defaultdict(int)
+    i = 0
+    for doc in word_freq_dict:
+        for word in doc:
+            idf[word] += 1
+    for word in idf:
+        idf[word] = math.log(len(idf)/(idf[word]+1))
+    return idf
 
-if __name__ == "__main__":
-    startTime = time.time()
+def gettfidf(doc_id):
     tra = TraversalFun("./1946年05月")
     # calc_idf("./1946年05月")
     corpus = tra.TraversalDir()
-    print("数据集大小，size: " + str(len(corpus)))
-    print(corpus)
-    word_tf, shannoEnt = calc_tf(corpus)
-    print("信息熵：" + str(shannoEnt))
-    print("TF: ")
-    # print(word_tf)
+    tf = calc_tf(corpus)
+    idf = calc_idf(tf)
+    if doc_id > len(tf):
+        print("超出文件个数")
+    else:
+        id_tf = tf[doc_id - 1]
+        for word in id_tf:
+            id_tf[word] = id_tf[word] * idf[word]
+        return id_tf
+
+def save(idf_dict, path):
+    '''
+    保存TF-IDF成文件
+    :param idf_dict:
+    :param path:
+    :return:
+    '''
+    f = open(path, 'a+')
+    # f = os.mknod(path, "a+")
+    f.truncate()
+    for key in idf_dict.keys():
+        f.write(str(key) + " " + str(idf_dict[key]) + "\n")
+    f.close()
+
+if __name__ == "__main__":
+    startTime = time.time()
+
+    tfidf = gettfidf(1)
+    print(tfidf)
+    save(tfidf, "./tfidf.txt")
     endTime = time.time()
     print("\ntotal cost time(s)", (endTime-startTime), 's')
